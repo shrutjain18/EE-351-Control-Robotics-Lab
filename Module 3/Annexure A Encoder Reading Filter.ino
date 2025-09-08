cd819ced9317374a8211550b0c6a0ea6eb387bb7
Shrut Jain <165655703+shrutjain18@users.noreply.github.com>
2025-09-08 10:05:34 +0530

    Update Annexure A Encoder Reading Filter.ino

--- Module 3/Annexure A Encoder Reading Filter.ino
+++ Module 3/Annexure A Encoder Reading Filter.ino
@@ -1,7 +1,7 @@
 const int encoderPinA = 2;
 const int encoderPinB = 3;
 unsigned long lastChangeTime = 0;
-const unsigned long debounceDelay = 500; // in microseconds
+const unsigned long debounceDelay = 100; // in microseconds
 int filteredA = 0;
 int lastStateA = 0;
 int filteredB = 0;
@@ -11,27 +11,27 @@ void setup(){
   Serial.begin(9600);  // Faster baud for better plotting
   pinMode(encoderPinA, INPUT_PULLUP);
   pinMode(encoderPinB, INPUT_PULLUP);
-
   Serial.println("Encoder signal monitor started...");
   }
 void loop() {
   int rawA = digitalRead(encoderPinA);
   int rawB = digitalRead(encoderPinB);
   unsigned long now = micros();
-
-  if (rawA != lastStateA) {
+  if (rawA != lastStateA || rawB != lastStateB) {
     if (now - lastChangeTime > debounceDelay) {
-      filteredA = rawA;             // accept new stable value
-      lastChangeTime = now;
-      filteredB = rawB;             // accept new stable value
+      filteredA = rawA;// accept new stable value
+      filteredB = rawB;
       lastChangeTime = now;
     }
     lastStateA = rawA;
     lastStateB = rawB;
   }
   // Plot both raw and filtered signals
-  Serial.print(rawA);
+  //Serial.print(rawA);
+  //Serial.print(" ");
+  Serial.print(filteredA);
+  //Serial.print(rawB);
   Serial.print(" ");
-  Serial.println(filteredA);
-  delay(10);
+  Serial.println(filteredB);
+  delay(1);
 }
